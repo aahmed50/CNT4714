67cc4964219c5a38be4b03630d647fef8cbe2bb3
Asma <79490748+aahmed50@users.noreply.github.com>
2023-03-01 22:53:07 -0500

    Start assignment 3
    initial setup + figuring out how to use sql again

---
+++ Project 3/src/Interface.java
@@ -0,0 +1,13 @@
+/* 
+  Name: Asma Ahmed
+  Course: CNT 4714 Spring 2023 
+  Assignment title: Project 3 – A Two-tier Client-Server Application 
+  Date:  March 9, 2023 
+ 
+  Class:  Interface
+*/ 
+
+
+public class Interface {
+
+}
